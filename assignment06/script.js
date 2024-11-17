49317497a900b2bdb7e3577466e8cc3cc63f85a3
briannaordaz <briannaordaz5@gmail.com>
2024-11-17 01:06:53 -0800

    added an if statement and a console.log to test out the code

--- assignment06/script.js
+++ assignment06/script.js
@@ -22,14 +22,21 @@ function reliableMultiply(a, b) {
   // Your code here.
   // we have to do a while loop so that if the code fails, it will allow us to keep trying to debug the correct code
   while(true) {
-    // then we add a try catch statement. the "try" will try out the code and the "catch" will catch the error
+    // then we add a try-catch statement. the "try" will try out the code and the "catch" will catch the error
     try {
-      // this part is an example of an error
-      return primitiveMultiply(5, 6);
-      // this will catch the error
+      // this will execute the code and give us the result
+      return primitiveMultiply(a, b); 
+      // this will catch the error if there is one
     } catch (error) {
-      
+      // this is an if statement, and this is what the output will be if there is an error in the code
+        if (!(error instanceof MultiplicatorUnitFailure)) {
+          // the "throw error" tells the system to throw an error if there is one
+          throw error;
+        }
     }
-    )
+    
 
   }
+}
+
+console.log(reliableMultiply(5, 6));  // output: 30
